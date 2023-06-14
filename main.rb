553973a4e7cd62a08752e3968235c31f85a5fd46
Lina <117147821+LinA636@users.noreply.github.com>
2023-06-15 09:42:34 +1200

    write driver code

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 require_relative 'chess_board.rb'
-require_relative 'knight.rb'
 
-chess = ChessBoard.new(8)
-knight1 = chess.knight
-p knight1.field.knight_children
+
+chess = ChessBoard.new()
+chess.knight_moves([0,0], [1,2])
+puts ''
+
+chess.knight_moves([0,0], [3,3])
+puts ''
+chess.knight_moves([3,3], [0,0])
+puts ''
+chess.knight_moves([3,3], [4,3])
