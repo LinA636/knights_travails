ef61abb26d672de73c90239ee87f0e4de3beee4a
Lina <117147821+LinA636@users.noreply.github.com>
2023-06-15 09:20:04 +1200

    implement Knight class with its possibile movements and to_s

--- knight.rb
+++ knight.rb
@@ -1,15 +1,29 @@
-require_relative 'field.rb'
-
 class Knight
-  attr_accessor :field, :next_moves
+  attr_accessor :children, :parent
+  attr_reader :board_size, :position
 
-  def initialize(field)
-    @field = field
-    @next_moves = calc_next_moves()
+  MOVEMENTS = [[-2,-1], [-2,1], [-1,-2], [-1,2], 
+              [1,-2], [1,2], [2,-1], [2,1]].freeze
+
+  def initialize(position, board_size = 8, parent = nil)
+    @position = position
+    @parent = parent
+    @board_size = board_size
+    @children = []
   end
 
-  def calc_next_moves()
+  def next_movements
+    MOVEMENTS.map{|arr| [self.position[0]+arr[0], self.position[1]+arr[1]]}
+             .keep_if{|arr| valid_pos?(arr, board_size)} 
+  end
 
+  def to_s
+    "Knight: #{self.position}"
+  end
+
+  private
+  def valid_pos?(arr, board_size)
+    arr[0].between?(0,board_size-1) && arr[1].between?(0,board_size-1)
   end
 
 end
